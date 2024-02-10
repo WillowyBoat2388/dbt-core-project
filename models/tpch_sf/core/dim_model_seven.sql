b42bf57bc241c01f8a4a648d18863baf92fb3793
Prince Fikayo <onidajo99@gmail.com>
2024-02-10 00:23:12 +0000

    -c Created a new model - model 7 of the organization's project

---
+++ models/tpch_sf/core/dim_model_seven.sql
@@ -0,0 +1,21 @@
+with n as(
+    select *
+    from {{ ref('stg_nation')}}
+),
+c as (
+    select *
+    from {{ ref('stg_customer')}}
+),
+o as (
+    select *
+    from {{ ref('stg_orders')}}
+),
+total_price as (
+        select n.name as nation, c.name as customer, o.orderdate as orderdate, sum(o.totalprice) as totalprice
+        from c
+        join n using (nationkey)
+        join o using (custkey)
+        group by n.name, c.name, o.orderdate
+)
+select nation, customer, orderdate, totalprice, ROUND(sum(totalprice) over ( partition by nation, customer order by orderdate ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW), 2) as running_total_price
+from total_price
